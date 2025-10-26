a35cde7f1ca997d7edd1ed239a3ca7702d0543e9
Luis Dionicio <ldionicio8888@gmail.com>
2025-10-26 18:35:10 -0500

    Pregunta 2 completa

--- PracticaExamenSegundoModulo/src/main/java/PreguntaDos/ListaDoblementeEnlazada.java
+++ PracticaExamenSegundoModulo/src/main/java/PreguntaDos/ListaDoblementeEnlazada.java
@@ -1,14 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package PreguntaDos;
 
-/**
- *
- * @author 51994
- */
 public class ListaDoblementeEnlazada {
 
+Nodo inicio;
+Nodo fin;
+
+    public void agregarInicio(Producto producto) {
+        Nodo nuevo = new Nodo(producto);
+        if (inicio == null) {
+            inicio = fin = nuevo;
+        } else {
+            nuevo.sig = inicio;
+            inicio.ant = nuevo;
+            inicio = nuevo;
+        }
+    }
+
+    public String imprimirAtrasAdelante() {
+        String resultado = "";
+        Nodo actual = fin;
+        while (actual != null) {
+            resultado += actual.valor.toString() + "\n";
+            actual = actual.ant;
+        }
+        return resultado;
+    }
+
+    public void eliminarUltimo() {
+        if (fin == null) {
+            System.out.println("Lista vacía, no se puede eliminar.");
+        } else if (inicio == fin) { 
+            inicio = fin = null;
+        } else {
+            fin = fin.ant;
+            fin.sig = null;
+        }
+    }
+
+    public void eliminarPrimero() {
+        if (inicio == null) {
+            System.out.println("Lista vacía, no se puede eliminar.");
+        } else if (inicio == fin) {
+            inicio = fin = null;
+        } else {
+            inicio = inicio.sig;
+            inicio.ant = null;
+        }
+    }
+        
 }
